79450df97e9b3a3055c3688b8d52a5afd89916f4
Andrei Cretu <86878513+ArduTalisman@users.noreply.github.com>
2025-03-25 09:33:28 +0200

    mmm

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,52 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.io.*;
+import java.nio.file.*;
+import java.util.*;
+
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        try {
+            String content= new String(Files.readAllBytes(Paths.get("src/in.txt")));
+            //a
+            String a = content.replaceAll("(?m)$", "\n");
+            //b
+            String b = content.replace(".", ".\n");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+            String c= a+ "\n"+ b;
+            //scriem in out.txt
+            Files.write(Paths.get("src/out.txt"), c.getBytes());
+
+            //afisare
+            System.out.println("rezultate a:\n"+a);
+            System.out.println("rezultate b:\n"+b);
+            //hhhh
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Random random= new Random();
+        List<String> lines= new ArrayList<>();
+        for(int i=0;i<5;i++){
+            List<String> words= new ArrayList<>();
+            for(int j=0;j<10;j++){
+                StringBuilder word= new StringBuilder();
+                for(int k=0;k<4;k++){
+                    char c= (char) (random.nextInt(26) + 'a');
+                    word.append(c);
+                }
+                words.add(word.toString());
+            }
+            Collections.sort(words);
+            lines.add(String.join(" ", words));
+
+
+            }
+        try {
+            Files.write(Paths.get("/Users/ardu/IdeaProjects/ingprog5/src/outrand.txt"),lines);
+        }catch (IOException e){
+            e.printStackTrace();
+
         }
     }
 }
